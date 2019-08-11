0f8ba2b039ecabf5fcdc856fe2bee76236ee7c5c
linqueta <lincolnrodrs@gmail.com>
2019-08-11 14:46:05 -0300

    set_turtle_test_environment

--- lib/turtle/event_notificator.rb
+++ lib/turtle/event_notificator.rb
@@ -10,7 +10,7 @@ module Turtle
       DEFAULT_ACTIONS = %i[create update destroy].freeze
 
       def act_as_notification(options)
-        return if defined?(Rails) && Rails.env.test?
+        return if defined?(Rails) && Rails.env.test? && !ENV['TURTLE_TEST']
         raise ModelRequiredError unless options[:model]
         return unless available_events?(options)
 
